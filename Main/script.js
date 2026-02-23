b457a2197c7aae996ac6cce98648254fcc5e3cfd
brandinpotter <Brandinpotter@gmail.com>
2026-02-22 22:27:50 -0500

    1

--- Main/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("btn");
-const msg = document.getElementById("msg");
-document.getElementById("year").textContent = new Date().getFullYear();
-
-btn.addEventListener("click", () => {
-  const now = new Date().toLocaleTimeString();
-  msg.textContent = `Button clicked at ${now}!`;
-});
